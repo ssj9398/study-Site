37d43ac3f8f61d3a0e724d7644bfd320f385544b
alisyabob <sjs939883@gmail.com>
2021-06-07 14:18:16 +0900

    전체 css, 경로, 스크롤 수정

--- Js/main.js
+++ Js/main.js
@@ -8,6 +8,20 @@ var search = document.querySelector(".search");
 var searchButton = search.querySelector("input[type=submit]");
 var scrollDown = document.querySelector("#scroll-down");
 
+var top = document.querySelector("#top");
+var bottom = document.querySelector("#bottom");
+
+top.addEventListener('click', (e) => {
+    e.stopPropagation();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+bottom.addEventListener('click', (e) => {
+    e.stopPropagation();
+    var height = document.body.scrollHeight;
+    window.scroll({ top: height, left: 0, behavior: 'smooth' });
+});
+
 toggleBtn.addEventListener('click', () => {
     console.log("aaa");
     menu.classList.toggle('active');
@@ -17,8 +31,4 @@ toggleBtn.addEventListener('click', () => {
 searchButton.addEventListener('click', () => {
     alert("검색영역 개발 중입니다.");
 
-});
-
-scrollDown.addEventListener('click', () => {
-    alert("a");
 });
